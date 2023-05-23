032c2e8a187f9bd20f39c3191b7ff43cd975a853
saicharanars <saicharan_@outlook.com>
2023-05-23 17:57:33 +0100

    bodyparser

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use((req,res,next)=>{
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST" ><label for="name">name</label><input type="text" id="name" name="name"><label for="size"> size</label><input type="text" id="size" name="size"><button type="submit">Submit</button></form>');
+    
+});
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
     console.log("in middleware");
     next();//allow request to continue next request
-});
-app.use((req,res,next)=>{
-    console.log("in next middleware");
-    res.send('<h1>hello from express</h1>');
 });
 app.listen(4000);
